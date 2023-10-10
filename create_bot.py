1ae750e1670a8fea56e467d0b4d0168affb9b265
Artemiy Zhuravlyov <yagaoffit@gmail.com>
2023-10-11 01:40:50 +0700

    Next level security

--- create_bot.py
+++ create_bot.py
@@ -1,11 +1,11 @@
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher 
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from secret import token, service_key_vk
 # from DB.database import Datebase
 
-BOT_TOKEN = '5512199707:AAE7MzQfIU2gtM6Gkg2_RQfnju5S3W385R0'
-SERVICE_KEY = 'f698fddbf698fddbf698fddb11f6e55dc5ff698f698fddb947f5587662c78409a9f6ffa'
-# ID_TRASH_CHANNEL = '-1001500845075'
+BOT_TOKEN = token
+SERVICE_KEY = service_key_vk
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage()) 
@@ -15,4 +15,3 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 vers = '5.131'
 url = 'https://vk.com/id'
 url_group = 'https://vk.com/'
-# password = '1ArtemLebedevMolodec!'
